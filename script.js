752a1f4f861ee4add2c60fdf8ad720397a06f0c7
RuslanPanasuk <panasuk.eetp@gmail.com>
2022-07-04 08:37:03 +0200

    feat: add video gallery section

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ document.querySelectorAll('.wrapper').forEach((item) => {
     });
   });
 });
+
+
+const videos = document.querySelectorAll('.video');
+
+videos.forEach((video) => {
+  video.addEventListener('mouseover', () => {
+    video.play();
+  });
+  video.addEventListener('mouseout', () => {
+    video.pause();
+  });
+});
